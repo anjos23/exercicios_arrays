b73690f75baad1404ae1c54c80385a626edf01e3
Thales.Anjos <129232556+anjos23@users.noreply.github.com>
2023-04-18 20:55:23 -0300

    exercicios arrays e strings

---
+++ exercicios_arrays/script.js
@@ -0,0 +1,20 @@
+let Array
+console.log('a.', Array)
+
+array = null// e um valor nulo
+console.log('b. ', array)
+
+array = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+console.log("c.", array.length)//mostra a quantidade
+
+//let i = 0
+//console.log('d.',array[i])//3
+
+let i = 0
+console.log('d.',array[i])//3
+
+array[i+1]=19;
+console.log('e',array)
+
+const valor = array[i+6]
+console.log('f.', valor)
